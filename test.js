56f2b2a97a23ca0aa04842e6206025913b9aebc1
Smanga Ngcamu <ngcamukhetha@gamil.com>
2024-04-02 11:03:47 +0200

    bind practice completed

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
 // Your code here
+const Employee = require('./employee');
+
+const emp = new Employee("John Wick", "Dog Lover");
+
+setTimeout(emp.sayName.bind(emp), 2000);
+setTimeout(emp.sayOccupation.bind(emp), 3000);
